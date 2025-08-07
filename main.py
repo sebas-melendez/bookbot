e362e80adbd1e68167e1978b362d26350699a9f8
sebas-melendez <sebastianmelendeztab@hotmail.com>
2025-08-07 12:40:01 -0600

    final version for upload

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+import sys
+from stats import get_num_words , char_stats, sort_chars
 def get_book_text(pathsito):
     with open(pathsito) as f:
         file_contents = f.read()
         return file_contents
 
-from stats import get_num_words , char_stats, sort_chars
 def main():
     textito = get_book_text(bookpath)
     print("======BOOKBOT======")
@@ -15,5 +16,12 @@ def main():
     for items in surtidito:
         print(f"{items["char"]}: {items["num"]}")
     return
-bookpath = "books/frankenstein.txt"
-main()
+
+if len(sys.argv) == 2:
+    bookpath = sys.argv[1]
+    main()
+else:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
+#books/frankenstein.txt
